98cbc458cf4ed4fcd5bed0fe6394704a1c263f7b
Enric Vazquez <vspevc@gmail.com>
2022-11-25 12:54:12 +0100

    Fix clean unused code in App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,24 +1,5 @@
-import { showModalActionCreator } from "../../redux/features/uiSlice/uiSlice";
-import { useAppDispatch, useAppSelector } from "../../redux/hooks";
-import Modal from "../Modal/Modal";
-
 const App = (): JSX.Element => {
-  const { modal } = useAppSelector(({ ui }) => ui);
-  const dispatch = useAppDispatch();
-
-  return (
-    <>
-      <div
-        onClick={() => {
-          dispatch(showModalActionCreator(<h2>Hola</h2>));
-        }}
-        className="container"
-      >
-        p:
-      </div>
-      {modal && <Modal children={modal} />}
-    </>
-  );
+  return <div className="container"></div>;
 };
 
 export default App;
