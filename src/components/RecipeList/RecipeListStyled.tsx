09c85cf44dbd7e1767cd06eeaf8b186395d74de2
Enric Vazquez <vspevc@gmail.com>
2022-12-01 00:02:06 +0100

    Add Home page test

--- src/components/RecipeList/RecipeListStyled.tsx
+++ src/components/RecipeList/RecipeListStyled.tsx
@@ -4,6 +4,7 @@ const RecipeListStyled = styled.div`
   .recipe-list {
     display: flex;
     flex-direction: column;
+    align-items: center;
     gap: ${(props) => props.theme.sizes.gap};
   }
 `;
