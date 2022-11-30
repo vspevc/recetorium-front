26019e9b859fefcce87c96c131f6f894044c53c7
Enric Vazquez <vspevc@gmail.com>
2022-11-30 18:41:44 +0100

    Add App test

---
+++ src/components/App/App.test.tsx
@@ -0,0 +1,33 @@
+import { screen } from "@testing-library/react";
+import { renderWithProviders } from "../../mocks/renderWithProviders";
+import { renderWithProvidersAndRouter } from "../../mocks/renderWithProvidersAndRouer";
+import { RecipeStructure } from "../../redux/features/recipesSlice/types";
+import { ModalStructure } from "../../redux/features/uiSlice/types";
+import App from "./App";
+
+describe("Given an App component", () => {
+  describe("When it's rendered", () => {
+    test("Then it should show", () => {
+      const expectedHeadingTitle = "Well done";
+      const expectedHeadingLevel = 2;
+      const modalIsOpen: ModalStructure = {
+        isOpen: true,
+        type: "success",
+        title: "Well done",
+      };
+
+      renderWithProvidersAndRouter(<App />, {
+        preloadedState: {
+          ui: { modal: modalIsOpen },
+          recipes: { recipes: {} as RecipeStructure[] },
+        },
+      });
+      const expectedModalHeading = screen.queryByRole("heading", {
+        name: expectedHeadingTitle,
+        level: expectedHeadingLevel,
+      });
+
+      expect(expectedModalHeading).toBeInTheDocument();
+    });
+  });
+});
