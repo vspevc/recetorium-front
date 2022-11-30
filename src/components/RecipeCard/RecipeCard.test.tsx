8a49d98a42198727c7b91f4343809cffe1e255d1
Enric Vazquez <vspevc@gmail.com>
2022-11-30 01:03:26 +0100

    Add RecipeCard component test

---
+++ src/components/RecipeCard/RecipeCard.test.tsx
@@ -0,0 +1,47 @@
+import { screen } from "@testing-library/react";
+import { renderWithProviders } from "../../mocks/renderWithProviders";
+import RecipeCard from "./RecipeCard";
+import { RecipeStructure } from "./types";
+
+describe("Given a RecipeCard component", () => {
+  const recipe: RecipeStructure = {
+    id: "",
+    name: "Yummy salad",
+    urlSlug: "/",
+    author: "",
+    types: [{ name: "desayuno" }, { name: "almuerzo" }, { name: "comida" }],
+    ingredients: [],
+    steps: [],
+    elaborationTime: "10 min",
+    image: "salad.jpg",
+  };
+
+  describe("When it's rendered with recipe name 'Yummy salad'", () => {
+    test("Then it should show a heading level 3 with 'Yummy salad'", () => {
+      const expectedHeadingLevel = 3;
+
+      renderWithProviders(<RecipeCard recipe={recipe} />);
+      const expectedHeading = screen.queryByRole("heading", {
+        name: recipe.name,
+        level: expectedHeadingLevel,
+      });
+
+      expect(expectedHeading).toBeInTheDocument();
+    });
+  });
+
+  describe("When it's rendered with recipe types 'cena' and 'postre'", () => {
+    test("Then it should show two tags with names 'cena' and 'postre'", () => {
+      const cena = "cena";
+      const postre = "postre";
+      recipe.types = [{ name: cena }, { name: postre }];
+
+      renderWithProviders(<RecipeCard recipe={recipe} />);
+      const expectedcena = screen.queryByText(cena);
+      const expectedpostre = screen.queryByText(postre);
+
+      expect(expectedcena).toBeInTheDocument();
+      expect(expectedpostre).toBeInTheDocument();
+    });
+  });
+});
