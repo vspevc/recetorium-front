364b369a482e2f1e72ad2fefea9a5443c7ff0068
Enric Vazquez <vspevc@gmail.com>
2022-12-06 16:36:26 +0100

    Add lazy loading

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,23 +1,32 @@
+import React, { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
-import CreateRecipe from "../../pages/CreateRecipe/CreateRecipe";
-import Home from "../../pages/Home/Home";
-import NotFound from "../../pages/NotFound/NotFound";
-import RegisterUser from "../../pages/RegisterUser/RegisterUser";
 import paths from "../../utils/paths/paths";
 import Header from "../Header/Header";
+import Loading from "../Loading/Loading";
 import LayoutStyled from "./LayoutStyled";
+
+const Home = React.lazy(() => import("../../pages/Home/Home"));
+const RegisterUser = React.lazy(
+  () => import("../../pages/RegisterUser/RegisterUser")
+);
+const CreateRecipe = React.lazy(
+  () => import("../../pages/CreateRecipe/CreateRecipe")
+);
+const NotFound = React.lazy(() => import("../../pages/NotFound/NotFound"));
 
 const Layout = (): JSX.Element => {
   return (
     <LayoutStyled>
       <Header />
       <main>
-        <Routes>
-          <Route path={paths.root} element={<Home />} />
-          <Route path={paths.registerUser} element={<RegisterUser />} />
-          <Route path={paths.createRecipe} element={<CreateRecipe />} />
-          <Route path={paths.notFound} element={<NotFound />} />
-        </Routes>
+        <Suspense fallback={<Loading />}>
+          <Routes>
+            <Route path={paths.root} element={<Home />} />
+            <Route path={paths.registerUser} element={<RegisterUser />} />
+            <Route path={paths.createRecipe} element={<CreateRecipe />} />
+            <Route path={paths.notFound} element={<NotFound />} />
+          </Routes>
+        </Suspense>
       </main>
     </LayoutStyled>
   );
